ab81e8b0850366185afc7124787190088344becd
cfinn7789 <cfinn7789@gmail.com>
2023-07-11 16:45:36 -0400

    updated the readme

--- Develop/script.js
+++ Develop/script.js
@@ -9,27 +9,24 @@ $(document).ready(function() { //Waits for the function to finish loading before
     localStorage.setItem(time, text); //Stores the value of the "text" variable into the web browser's local storage, using the "time" variable as the key
   });
 
-//YO the screw up might be the for loop maybe we're not translating it to parseInt correctly. Note to self.
 function timeTracker(){
   var currentTime = dayjs().format('H'); //Retrieves the current hour by using the hour() function provided by dayjs
-
-  for (var i = 8; i <= 17; i++) { //Initializes variable "i" and increments it by 1 until it reaches 17
-    var hour = ".hour" + i; //Concatenates the string "#hour" with the current value of "i"
-    var timeRow = parseInt($(this).attr("id").split("hour")[1]); //Retrieves the value of the id attribute then splits it into an array of substrings and converts it into an integer
-  console.log(timeRow, currentTime);
-
-  $(hour + " .description").val(localStorage.getItem("hour" + i)); //Uses the constructed hour variable to select an element with the class "description"
+  var timeRow = parseInt($(this).attr("id").split("hour")[1]); //Retrieves the value of the id attribute then splits it into an array of substrings and converts it into an integer
+  console.log(timeRow, currentTime); //Logs the timeRow and currentTime to check if they are working properly
   
+  for (var i = 8; i < 18; i++) { //Initializes variable "i" and increments it by 1 until it reaches 17
+    var hour = "#hour" + i; //Concatenates the string "#hour" with the current value of "i"
+    $(hour + ".description").val(localStorage.getItem(hour)); //Uses the constructed hour variable to select an element with the class "description"
 
   if(timeRow < currentTime){ //Checks if the value of timeRow is less than currentTime and if so, adds a class of "past"
     $(this).addClass("past");
     $(this).removeClass("present");
     $(this).removeClass("future");
-  } else if(timeRow === currentTime) {
+  } else if(timeRow === currentTime) { //Checks if the value of timeRow is the same as currentTime and adds a class of present if true
     $(this).removeClass("past");
     $(this).addClass("present");
     $(this).removeClass("future");
-  } else if (timeRow > currentTime){
+  } else if (timeRow > currentTime){ //Checks if the value of timeRow is greater than timeRow and adds a class of future if true
     $(this).removeClass("past");
     $(this).removeClass("present");
     $(this).addClass("future");
@@ -38,24 +35,3 @@ function timeTracker(){
 }
 $(".time-block").each(timeTracker);
 });
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Work-Day-Scheduler
-A scheduler to keep track of projects
+# Work Day Scheduler
+
+This project was created to track and schedule your work day. It saves the data inputted into the text boxes as keys for future viewing. I learned more about how bootstrap, jQuery, and Dayjs worked in this project, and how they can interact with a webpage.
+
+# Installation
+
+N/A
+
+# Usage
+
+This project is to be used to keep track of your work day.
+
+# Image of the live website
+
+![Image of the Live Website](_C__Users_Chelsey_Documents_UCF_homework_work-day-scheduler_Work-Day-Scheduler_Develop_index.html.png)
+
+
+# Links
+
+[My Github](https://github.com/cfinn7789/)
+[The website](https://cfinn7789.github.io//)
